390bbdf54a784ddda41ec19f45871ed4b15c8f44
Yurii Bezruk <yurii.bezruk.it@gmail.com>
2022-02-01 22:07:32 +0200

    Added one stage of evolution modelling

--- main.c
+++ main.c
@@ -27,6 +27,18 @@ int main() {
         }
     }
     for (int i = 0; i < all_strategies_count; i++){
+        for (int j = i; j < all_strategies_count; j++){
+            for (int k = 0; k < ITERATIONS_COUNT; k++){
+                int s1_move = bit_at(strategies[i].name, strategies[j].prev_move);
+                int s2_move = bit_at(strategies[j].name, strategies[i].prev_move);
+                strategies[i].points += matrix[s1_move][s2_move];
+                strategies[j].points += matrix[s2_move][s1_move];
+                append_move(strategies[i].prev_move, s1_move, sub_digits_count);
+                append_move(strategies[j].prev_move, s2_move, sub_digits_count);
+            }
+        }
+    }
+    for (int i = 0; i < all_strategies_count; i++){
         println_strategy(strategies + i, main_digits_count);
     }
 }
